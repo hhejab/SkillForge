370d6ffef10892b25c9290895cebe5dc09c90c56
hhejab <202301136@student.polytechnic.bh>
2026-05-16 17:37:56 +0300

    Added and tested API controllers

---
+++ SkillForge.API/Controllers/VerificationStatusController.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SkillForge.API.Models;
+
+namespace SkillForge.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class VerificationStatusController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public VerificationStatusController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/VerificationStatus
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<VerificationStatus>>> GetVerificationStatuses()
+        {
+            return await _context.VerificationStatuses.ToListAsync();
+        }
+
+        // GET: api/VerificationStatus/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<VerificationStatus>> GetVerificationStatus(int id)
+        {
+            var verificationStatus = await _context.VerificationStatuses.FindAsync(id);
+
+            if (verificationStatus == null)
+            {
+                return NotFound();
+            }
+
+            return verificationStatus;
+        }
+
+        // PUT: api/VerificationStatus/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutVerificationStatus(int id, VerificationStatus verificationStatus)
+        {
+            if (id != verificationStatus.VerificationStatusId)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(verificationStatus).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!VerificationStatusExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/VerificationStatus
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<VerificationStatus>> PostVerificationStatus(VerificationStatus verificationStatus)
+        {
+            _context.VerificationStatuses.Add(verificationStatus);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetVerificationStatus", new { id = verificationStatus.VerificationStatusId }, verificationStatus);
+        }
+
+        // DELETE: api/VerificationStatus/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteVerificationStatus(int id)
+        {
+            var verificationStatus = await _context.VerificationStatuses.FindAsync(id);
+            if (verificationStatus == null)
+            {
+                return NotFound();
+            }
+
+            _context.VerificationStatuses.Remove(verificationStatus);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool VerificationStatusExists(int id)
+        {
+            return _context.VerificationStatuses.Any(e => e.VerificationStatusId == id);
+        }
+    }
+}
